951686c7d3c5db6494470fccfb05d4fc455fcda6
Chris Hyndman <chrishyndman@hey.com>
2022-01-30 18:31:50 -0800

    month-based sun time, fix array new

--- SkylightFxEngine/Thread.fs
+++ SkylightFxEngine/Thread.fs
@@ -9,9 +9,27 @@ open OpenRGB.NET
 
 let run (param: obj) =
     let getTod () =
-        let tod = DateTime.Now.TimeOfDay
-        System.Diagnostics.Debug.WriteLine("{0}", tod)
-        tod
+        let now = DateTime.Now
+        System.Diagnostics.Debug.WriteLine("{0}", now.TimeOfDay)
+        let nowAdj = if now.IsDaylightSavingTime() then now.AddHours(-1.0) else now
+        nowAdj.TimeOfDay
+
+    let sunTimeMonths = Map [
+        (1, ((6, 52), (16, 52)))
+        (2, ((6, 44), (17, 21)))
+        (3, ((6, 16), (17, 46)))
+        (4, ((5, 36), (18, 9)))
+        (5, ((5, 1), (18, 31)))
+        (6, ((4, 40), (18, 53)))
+        (7, ((4, 43), (19, 2)))
+        (8, ((5, 2), (18, 48)))
+        (9, ((5, 23), (18, 13)))
+        (10, ((5, 43), (17, 33)))
+        (11, ((6, 7), (16, 57)))
+        (12, ((6, 34), (16, 44)))
+        ]
+
+    let ((srh, srm), (ssh, ssm)) = sunTimeMonths.[DateTime.Now.Month]
 
     let skyParam: Scenes.SkyScene.Param =
         { Levels =
@@ -28,16 +46,15 @@ let run (param: obj) =
                 SunHigh = 255, 178, 0
                 SunLow = 209, 55, 0 }
           SunTimes =
-              { Sunrise = new TimeSpan(6, 0, 0)
-                Sunset = new TimeSpan(19, 0, 0) }
+              { Sunrise = new TimeSpan(srh, srm, 0)
+                Sunset = new TimeSpan(ssh, ssm, 0) }
           FramePeriodMsec = (15 * 1000)
           GetTimeOfDay = getTod }
 
     let devColors =
-        [
-            Array.create 5 (Models.Color());
-            Array.create 6 (Models.Color())
-        ]
+        let newColors n = Array.init n (fun _ -> new Models.Color())
+        [ 5; 6 ]
+        |> List.map newColors
 
     let handleColorSet ((r, g, b): Common.Color, addrs: Common.LedAddress list) =
         let updateDevColor (devColor: Models.Color) =
@@ -62,7 +79,6 @@ let run (param: obj) =
         with
             _ -> ()
         period
-
 
     let wh = (param :?> CancellationToken).WaitHandle
     while not (wh.WaitOne (handleFrame())) do ()
